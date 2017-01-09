805742312ce32f49ff02c47e15bf6bd78c8a0e5c
Athan Clark <athan.clark@gmail.com>
2017-01-08 18:59:53 -0800

    units of magnitude

--- src/Formula/Magnitude.hs
+++ src/Formula/Magnitude.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE
+    GeneralizedNewtypeDeriving
+  #-}
+
 
 {-|
 
@@ -7,40 +11,69 @@ https://en.wikipedia.org/wiki/Metric_prefix
 
 module Formula.Magnitude where
 
+-- | e18
+newtype Exa   a = Exa   {getExa   :: a}
+  deriving (Num, Eq, Ord)
 
-newtype Exa   a = Exa   {getExa   :: a} --            1000000000000000000 	1018
+-- | e15
+newtype Peta  a = Peta  {getPeta  :: a}
   deriving (Num, Eq, Ord)
-newtype Peta  a = Peta  {getPeta  :: a} --            1000000000000000 	1015
+
+-- | e12
+newtype Tera  a = Tera  {getTera  :: a}
   deriving (Num, Eq, Ord)
-newtype Tera  a = Tera  {getTera  :: a} --            1000000000000 	1012
+
+-- | e9
+newtype Giga  a = Giga  {getGiga  :: a}
   deriving (Num, Eq, Ord)
-newtype Giga  a = Giga  {getGiga  :: a} --            1000000000 	109
+
+-- | e6
+newtype Mega  a = Mega  {getMega  :: a}
   deriving (Num, Eq, Ord)
-newtype Mega  a = Mega  {getMega  :: a} --            1000000 	106
+
+-- | e3
+newtype Kilo  a = Kilo  {getKilo  :: a}
   deriving (Num, Eq, Ord)
-newtype Kilo  a = Kilo  {getKilo  :: a} --            1000 	103
+
+-- | e2
+newtype Hecto a = Hecto {getHecto :: a}
   deriving (Num, Eq, Ord)
-newtype Hecto a = Hecto {getHecto :: a} --            100 	102
-  deriving (Num, Eq, Ord)
-newtype Deca  a = Deca  {getDeca  :: a} --            10 	101
+
+-- | e1
+newtype Deca  a = Deca  {getDeca  :: a}
   deriving (Num, Eq, Ord)
 
 -- | The "no-op" unit - @*1@
-newtype Unit a = Unit {getUnit :: a}
+newtype Mono  a = Mono  {getMono :: a}
 
-newtype Deci  a = Deci  {getDeci  :: a}    -- d0.1 	10−1
+-- | e−1
+newtype Deci  a = Deci  {getDeci  :: a}
   deriving (Num, Eq, Ord)
-newtype Centi a = Centi {getCenti :: a}    -- c0.01 	10−2
+
+-- | e−2
+newtype Centi a = Centi {getCenti :: a}
   deriving (Num, Eq, Ord)
-newtype Milli a = Milli {getMilli :: a}    -- m0.001 	10−3
+
+-- | e−3
+newtype Milli a = Milli {getMilli :: a}
   deriving (Num, Eq, Ord)
-newtype Micro a = Micro {getMicro :: a}    -- μ0.000001 	10−6
+
+-- | e−6
+newtype Micro a = Micro {getMicro :: a}
   deriving (Num, Eq, Ord)
-newtype Nano  a = Nano  {getNano  :: a}    -- n0.000000001 	10−9
+
+-- | e−9
+newtype Nano  a = Nano  {getNano  :: a}
   deriving (Num, Eq, Ord)
-newtype Pico  a = Pico  {getPico  :: a}    -- p0.000000000001 	10−12
+
+-- | e−12
+newtype Pico  a = Pico  {getPico  :: a}
   deriving (Num, Eq, Ord)
-newtype Femto a = Femto {getFemto :: a}    -- f0.000000000000001 	10−15
+
+-- | e−15
+newtype Femto a = Femto {getFemto :: a}
   deriving (Num, Eq, Ord)
-newtype Atto  a = Atto  {getAtto  :: a}    -- a0.000000000000000001 	10−18
+
+-- | e−18
+newtype Atto  a = Atto  {getAtto  :: a}
   deriving (Num, Eq, Ord)
